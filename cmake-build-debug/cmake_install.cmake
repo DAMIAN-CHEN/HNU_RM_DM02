cf18f23ff8bef5c51ebae74a7e68f4f1c59b5c11
DAMIAN_CHEN <920296082@qq.com>
2024-10-25 22:33:13 +0800

    [update]: DSP

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/CubeMX_project/HNU_RM_DM02
+# Install script for directory: D:/GitHub/HNU_RM_DM02
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/CubeMX_project/HNU_RM_DM02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/GitHub/HNU_RM_DM02/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
